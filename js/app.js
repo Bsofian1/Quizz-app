6f47f6373bc3879456f6f5868e932dc47a78458b
Sofian <bsofianb1@gmail.com>
2021-02-25 17:13:46 +0100

    main logci css all good

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const App=  (()=> {
 //Cache the DOM
 
 const quizEl = document.querySelector(".container-quiz");
-const quizQuestionEl= document.querySelector(".question-box");
+const quizQuestionEl= document.querySelector(".question");
 const trackerEl= document.querySelector(".tracker");
 const tagLineEl= document.querySelector(".instruction")
 const choicesEl= document.querySelector(".choices")
@@ -14,7 +14,7 @@ const nextButtonEl = document.querySelector(".next")
 const restartButtonEl = document.querySelector(".restart")
 
 const q1= new Question(
-    "First president of the US"
+    "First president of the US",
     ["Barrack", "Osoma", "George", "boo"],
     2
     )
@@ -44,9 +44,72 @@ const q6= new Question(
     0
     )
 const quiz = new Quiz([q1, q2, q3, q4, q5, q6]);
-console.log(quiz)
+
+const setValue = (elem, value) => {
+    elem.innerHTML = value;
+}
+
+const renderQuestion= () => {
+    const question = quiz.getCurrentQuestion().question;
+    setValue(quizQuestionEl, question)
+}
+
+const renderChoicesElem =() => {
+    let markup= "";
+    const currentChoices= quiz.getCurrentQuestion().choices;
+    currentChoices.forEach((item, index) => {
+        markup +=`<li class="choice">
+        <input type="radio" name="input" class="input" id="choice${index}" />
+        <label for="choice${index}" class="label">
+          <i></i>
+          ${item}
+        </label>
+      </li>`
+    });
+    setValue(choicesEl, markup)
+};
+
+const renderTracker= ()=> {
+    const currentQuestionIndex = quiz.index;
+    setValue(trackerEl, `${currentQuestionIndex+1}/${quiz.questions.length}`)
+}
+const getPercent = (a,b)=> {
+    return Math.round((a/b)*100)
+}
+
+const launch = (width, max) => {
+    let loadingBar= setInterval(function(){
+        if(width > max){
+            clearInterval(loadingBar)
+        }else{
+            width++
+            progressInner.style.width = `${width}%`
+        }
+    },3)
+}
+
+const renderProgress= ()=> {
+    const progressIndex = getPercent(quiz.index, quiz.questions.length)
+    launch(0, progressIndex)
+}
+
+
+
+const renderAll =_=> {
+    if(quiz.hasEnded()){
+    //render endScreen
+    }else{
+    renderQuestion();
+    renderChoicesElem();
+    renderTracker();
+    renderProgress()
+    }
+}
+
+return {
+    renderAll: renderAll,
+}
 
 })();
 
-
-
+App.renderAll();
